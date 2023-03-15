b24ef0f17cef14a6901c80a3c4d1e5353911dd56
ahmad sidaoui <ahmadsidaoui@ahmads-Air.home>
2023-03-15 18:35:02 +0100

    updated

---
+++ Queries.sql
@@ -0,0 +1,124 @@
+-- Number of retiring employees
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_day BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_day BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_day BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+DROP TABLE retirement_info;
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_day BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+	managers.emp_no,
+ 	managers.from_date,
+	managers.to_date
+FROM departments
+INNER JOIN managers
+ON departments.dept_no = managers.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- The current employees who are eligible for retirement
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT Join dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Grouping the number employees who are eligible for retrirement by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirement_info_by_department
+FROM current_emp AS ce
+LEFT JOIN dept_emp AS de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Employee information about salaries
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON(e.emp_no = de.emp_no)
+WHERE (e.birth_day BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of active managers per department
+SELECT dm.dept_no,
+ 	d.dept_name,
+ 	dm.emp_no,
+ 	ce.last_name,
+ 	ce.first_name,
+ 	dm.from_date,
+ 	dm.to_date
+INTO manager_info
+FROM managers AS dm
+INNER JOIN departments AS d
+ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp AS ce
+ON (dm.emp_no = ce.emp_no);
+
+-- Current employees that will retire (including their corresponding department)
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Information relevant to sales team 
+SELECT emp_no,
+	first_name,
+	last_name,
+	dept_name
+INTO sales_team_info
+FROM dept_info
+WHERE dept_name = 'Sales';
+
+-- Information relevant to sales team and Development team
+SELECT emp_no,
+	first_name,
+	last_name,
+	dept_name
+INTO sales_dev_teams_info
+FROM dept_info
+WHERE dept_name IN ('Sales', 'Development')
